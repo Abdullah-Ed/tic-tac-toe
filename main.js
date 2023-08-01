a5fbca4c8d5696250c2186689dca1a5d1d1904b1
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-08-01 21:40:26 +0200

    Add winning condition

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const printGameLayout = (()=> {
+const printGameLayout = (() => {
   let gameBoard = {
     gameBoardArray: ['', '', '', '', '', '', '', '', ''],
   };
@@ -19,32 +19,82 @@ const createPlayers = (() => {
   return { players };
 })();
 
-
 (function render(gameBoard) {
   const container = document.querySelector('.container');
-  let arrayLength = gameBoard.gameBoardArray.length
-  for(let i = 0 ; i < arrayLength; i++){
+  let arrayLength = gameBoard.gameBoardArray.length;
+  for (let i = 0; i < arrayLength; i++) {
     const div = document.createElement('div');
     div.textContent = gameBoard.gameBoardArray[i];
-    div.classList.add('gameBox')
+    div.classList.add('gameBox');
     container.appendChild(div);
-  };
-})( printGameLayout.gameBoard);
+  }
+})(printGameLayout.gameBoard);
 
+const divElements = document.querySelectorAll('.gameBox');
 
 (function gameEvent(players, gameBoard) {
-  const divElements = document.querySelectorAll('.gameBox');
   divElements.forEach((div, index) => {
     div.addEventListener('click', () => {
+      if (!gameResult == ''){
+        return;
+      }
+
+      let X = 0;
+      let O = 0;
+      for (let i = 0; i < gameBoard.gameBoardArray.length; i++) {
+        if (gameBoard.gameBoardArray[i] == 'X') {
+          X++;
+        } else if (gameBoard.gameBoardArray[i] == 'O') {
+          O++;
+        }
+      }
+
       if (gameBoard.gameBoardArray[index] === '') {
-        if (index % 2 === 0) {
+        if (X <= O) {
           div.textContent = players[0].symbol;
           gameBoard.gameBoardArray[index] = players[0].symbol;
         } else {
-          div.textContent = players[1].symbol; // Change to players[1].symbol
-          gameBoard.gameBoardArray[index] = players[1].symbol; // Change to players[1].symbol
+          div.textContent = players[1].symbol;
+          gameBoard.gameBoardArray[index] = players[1].symbol;
         }
       }
+ 
+      checkGame(gameBoard); 
     });
   });
 })(createPlayers.players, printGameLayout.gameBoard);
+
+let gameResult = ''
+function checkGame(gameBoard) {
+
+  let gameBoardArray = gameBoard.gameBoardArray;
+  if ((gameBoardArray[0] === 'X' && gameBoardArray[1] === 'X' && gameBoardArray[2] === 'X')||
+      (gameBoardArray[3] === 'X' && gameBoardArray[4] === 'X' && gameBoardArray[5] === 'X')||
+      (gameBoardArray[6] === 'X' && gameBoardArray[7] === 'X' && gameBoardArray[8] === 'X')||
+      
+      (gameBoardArray[0] === 'X' && gameBoardArray[3] === 'X' && gameBoardArray[6] === 'X')||
+      (gameBoardArray[1] === 'X' && gameBoardArray[4] === 'X' && gameBoardArray[7] === 'X')||
+      (gameBoardArray[2] === 'X' && gameBoardArray[5] === 'X' && gameBoardArray[8] === 'X')||
+      
+      (gameBoardArray[2] === 'X' && gameBoardArray[4] === 'X' && gameBoardArray[6] === 'X')||
+      (gameBoardArray[0] === 'X' && gameBoardArray[4] === 'X' && gameBoardArray[8] === 'X')) {
+      console.log('player X Won');
+      gameResult = 'X won ';
+  } else if(
+      (gameBoardArray[0] === 'O' && gameBoardArray[1] === 'O' && gameBoardArray[2] === 'O')||
+      (gameBoardArray[3] === 'O' && gameBoardArray[4] === 'O' && gameBoardArray[5] === 'O')||
+      (gameBoardArray[6] === 'O' && gameBoardArray[7] === 'O' && gameBoardArray[8] === 'O')||
+      
+      (gameBoardArray[0] === 'O' && gameBoardArray[3] === 'O' && gameBoardArray[6] === 'O')||
+      (gameBoardArray[1] === 'O' && gameBoardArray[4] === 'O' && gameBoardArray[7] === 'O')||
+      (gameBoardArray[2] === 'O' && gameBoardArray[5] === 'O' && gameBoardArray[8] === 'O')||
+      
+      (gameBoardArray[2] === 'O' && gameBoardArray[4] === 'O' && gameBoardArray[6] === 'O')||
+      (gameBoardArray[0] === 'O' && gameBoardArray[4] === 'O' && gameBoardArray[8] === 'O')) {
+      console.log('player O Won');
+      gameResult = 'O won ';
+  } else if (!gameBoardArray.includes('')){
+    console.log('Tie')
+    gameResult = 'no one  won ';
+  }
+}
